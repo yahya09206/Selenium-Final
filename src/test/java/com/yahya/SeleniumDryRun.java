a56491c69f4871e59f3fef9fa58c19ae39bbfdf8
Yahya <yahyahussein91@gmail.com>
2024-04-05 11:01:42 -0700

    set up webdriver class to navigate to google

--- src/test/java/com/yahya/SeleniumDryRun.java
+++ src/test/java/com/yahya/SeleniumDryRun.java
@@ -1,7 +1,20 @@
 package com.yahya;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumDryRun {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1. Setup Chrome Driver so Selenium can use it to automate chrome
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("google.com");
+
+        Thread.sleep(2);
+
+        driver.quit();
 
 
     }
