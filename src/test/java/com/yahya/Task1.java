c63196bbb083cae03aef419d644a4f1bce52514f
Yahya <yahyahussein91@gmail.com>
2024-04-05 11:12:51 -0700

    task one set up

--- src/test/java/com/yahya/Task1.java
+++ src/test/java/com/yahya/Task1.java
@@ -4,18 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumDryRun {
+public class Task1 {
     public static void main(String[] args) throws InterruptedException {
 
-        //1. Setup Chrome Driver so Selenium can use it to automate chrome
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://google.com");
+        driver.navigate().to("https://facebook.com");
 
-        Thread.sleep(200);
-
+        Thread.sleep(3000);
         driver.quit();
-
-
     }
 }
