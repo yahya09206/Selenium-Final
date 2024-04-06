a77dbb27644534fa3cf79625ee491141df5a63a9
Yahya <yahyahussein91@gmail.com>
2024-04-06 15:08:05 -0700

    set up webdriver

---
+++ src/test/java/com/yahya/day02_locators_practice/FindByPartialTextAndGetText_Method.java
@@ -0,0 +1,14 @@
+package com.yahya.day02_locators_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindByPartialTextAndGetText_Method {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cydeo.com/");
+    }
+}
