6b664be6080d6ef4c9ae89bbd5fea8568fbd7b7d
Yahya <yahyahussein91@gmail.com>
2024-04-06 15:00:33 -0700

    clicking on first link

--- src/test/java/com/yahya/day02_locators_practice/LinkPartialTextLinkText.java
+++ src/test/java/com/yahya/day02_locators_practice/LinkPartialTextLinkText.java
@@ -1,14 +1,27 @@
 package com.yahya.day02_locators_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkPartialTextLinkText {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cydeo.com/");
+
+        // Click first link A/B testing
+        driver.findElement(By.linkText("A/B Testing")).click();
+        // Navigate back to home page
+        // Click on second link
+        // Click on Home link to go back to home
+        // Click on redirect link
+
+        Thread.sleep(3000);
+        driver.quit();
+
     }
 }
