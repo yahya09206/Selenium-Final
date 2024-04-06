70400e49889399859750dbec5fbb5d9736a710c3
Yahya <yahyahussein91@gmail.com>
2024-04-06 15:11:49 -0700

    added headless mode

--- src/test/java/com/yahya/day02_locators_practice/FindByPartialTextAndGetText_Method.java
+++ src/test/java/com/yahya/day02_locators_practice/FindByPartialTextAndGetText_Method.java
@@ -5,17 +5,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FindByPartialTextAndGetText_Method {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("http://practice.cydeo.com/");
 
         // Identify the link with text "Disappearing Elements"
         // using partial link text then get the full text using new method getText
         WebElement myLink = driver.findElement(By.partialLinkText("Disappearing"));
         System.out.println("myLink.getText() = " + myLink.getText());
+
+        driver.quit();
     }
 }
