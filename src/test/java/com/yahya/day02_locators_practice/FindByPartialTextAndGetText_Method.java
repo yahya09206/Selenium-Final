f34e1ec0267f05e225e21b2d193d9cfacc116344
Yahya <yahyahussein91@gmail.com>
2024-04-06 15:09:31 -0700

    getting full text by using gettext method

--- src/test/java/com/yahya/day02_locators_practice/FindByPartialTextAndGetText_Method.java
+++ src/test/java/com/yahya/day02_locators_practice/FindByPartialTextAndGetText_Method.java
@@ -1,7 +1,9 @@
 package com.yahya.day02_locators_practice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindByPartialTextAndGetText_Method {
@@ -10,5 +12,10 @@ public class FindByPartialTextAndGetText_Method {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cydeo.com/");
+
+        // Identify the link with text "Disappearing Elements"
+        // using partial link text then get the full text using new method getText
+        WebElement myLink = driver.findElement(By.partialLinkText("Disappearing"));
+        System.out.println("myLink.getText() = " + myLink.getText());
     }
 }
