a8ea447005ab527c7bba727367d2075637e9c5d3
Yahya <yahyahussein91@gmail.com>
2024-04-07 15:14:44 -0700

    getting attribute of first link item

--- src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
+++ src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -1,7 +1,9 @@
 package com.yahya.day03_locate_by_tag_class_getatt;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetElementAttribute {
@@ -11,5 +13,10 @@ public class GetElementAttribute {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cydeo.com/");
+
+        // Get the first link on the page and get href attribute
+        WebElement firstLink = driver.findElement(By.linkText("A/B Testing"));
+        System.out.println(firstLink.getText());
+
     }
 }
