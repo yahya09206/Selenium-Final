3e3fde77153749972e11a1174478c412d051b11c
Yahya <yahyahussein91@gmail.com>
2024-04-07 15:20:03 -0700

    getting href att from first link

--- src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
+++ src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -5,18 +5,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GetElementAttribute {
     public static void main(String[] args) {
 
         // Set up chrome driver
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://practice.cydeo.com/");
 
         // Get the first link on the page and get href attribute
         WebElement firstLink = driver.findElement(By.linkText("A/B Testing"));
         System.out.println(firstLink.getText());
+
+        // Get any attribute identified element, in this case href
+        System.out.println("firstLink.getAttribute(\"href\") = " + firstLink.getAttribute("href"));
+
+        driver.quit();
+
 
     }
 }
