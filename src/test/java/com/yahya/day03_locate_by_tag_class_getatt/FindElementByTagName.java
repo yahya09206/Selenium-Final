7e3649ac4a8c545d30150c753f673bb1c2d36077
Yahya <yahyahussein91@gmail.com>
2024-04-07 15:42:53 -0700

    getting text and name att

---
+++ src/test/java/com/yahya/day03_locate_by_tag_class_getatt/FindElementByTagName.java
@@ -0,0 +1,39 @@
+package com.yahya.day03_locate_by_tag_class_getatt;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class FindElementByTagName {
+    public static void main(String[] args) {
+
+        /**
+         * Purple = tag name
+         * Orange = attribute name
+         * Blue = attribute value
+         * Black = text value
+         */
+
+        // Open chrome and navigate to practice website
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://practice.cydeo.com/");
+        // Identify first element with tag name A
+        WebElement firstLink = driver.findElement(By.tagName("a"));
+        System.out.println("firstLink.getText() = " + firstLink.getText());
+
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        System.out.println("allLinks.size() = " + allLinks.size());
+        for (WebElement allLink : allLinks) {
+            System.out.println("allLink.getText() = " + allLink.getText());
+            System.out.println("allLink.getAttribute(\"a\") = " + allLink.getAttribute("href"));
+        }
+    }
+}
