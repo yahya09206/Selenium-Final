0f58daab2962df0bc690b2f4fb9b6e17ffd6841b
Yahya <yahyahussein91@gmail.com>
2024-04-07 15:13:28 -0700

    set up chrome driver

---
+++ src/test/java/com/yahya/day03_locate_by_tag_class_getatt/GetElementAttribute.java
@@ -0,0 +1,15 @@
+package com.yahya.day03_locate_by_tag_class_getatt;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetElementAttribute {
+    public static void main(String[] args) {
+
+        // Set up chrome driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cydeo.com/");
+    }
+}
