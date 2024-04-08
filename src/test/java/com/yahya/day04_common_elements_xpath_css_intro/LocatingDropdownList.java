24982198d11ddff18ed52d15ca0c064c3f94e2fa
Yahya <yahyahussein91@gmail.com>
2024-04-08 11:15:48 -0700

    selecting month dropdown options

--- src/test/java/com/yahya/day04_common_elements_xpath_css_intro/LocatingDropdownList.java
+++ src/test/java/com/yahya/day04_common_elements_xpath_css_intro/LocatingDropdownList.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
 public class LocatingDropdownList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -24,12 +24,29 @@ public class LocatingDropdownList {
         // TODO: Select items in 3 different ways
         // Select 3rd item
         isSelected.selectByIndex(2);
+        Thread.sleep(3000);
         // Select item with value attribute 2019
         isSelected.selectByValue("2019");
+        Thread.sleep(3000);
         // Select item with visible text 1990
         isSelected.selectByVisibleText("1990");
-        // Wrong info
-        isSelected.selectByVisibleText("1990");
+        Thread.sleep(3000);
+
+        System.out.println("-------------------------------");
+        // TODO: Identify the month dropdown
+        WebElement monthDropdown = driver.findElement(By.id("month"));
+        // TODO: Wrap it up using Select class so we can use easy methods
+        Select monthDropdownObj = new Select(monthDropdown);
+        // TODO: Select items in 3 different ways
+        // Select 3rd item
+        monthDropdownObj.selectByIndex(2);
+        Thread.sleep(3000);
+        // Select item with value attribute 2019
+        monthDropdownObj.selectByValue("7");
+        Thread.sleep(3000);
+        // Select item with visible text 1990
+        monthDropdownObj.selectByVisibleText("June");
+        Thread.sleep(3000);
 
         driver.quit();
 
