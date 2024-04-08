481d994f5fb4886e06b6f0c9be8bf0df45800da1
Yahya <yahyahussein91@gmail.com>
2024-04-08 11:26:23 -0700

    selecting links to open from unusual dropdown

---
+++ src/test/java/com/yahya/day04_common_elements_xpath_css_intro/SelectingFromFakeDropdown.java
@@ -0,0 +1,25 @@
+package com.yahya.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectingFromFakeDropdown {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cydeo.com/dropdown");
+
+        // The only way that Select class works, is if element tag is "select"
+        WebElement unusualDropdown = driver.findElement(By.id("dropdownMenuLink"));
+        unusualDropdown.click();
+        // Identify and click on google option
+        driver.findElement(By.linkText("Google")).click();
+
+        driver.quit();
+    }
+}
