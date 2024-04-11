d76da99817019d729659feab82598c5939d07324
Yahya <yahyahussein91@gmail.com>
2024-04-11 12:44:00 -0700

    using xpath to click checkboxes

--- src/test/java/com/yahya/day04_common_elements_xpath_css_intro/LocatingByXpathIntro.java
+++ src/test/java/com/yahya/day04_common_elements_xpath_css_intro/LocatingByXpathIntro.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatingByXpathIntro {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // /html/body/div[1]/div[2]/div/div/div/h1/span
 
@@ -17,6 +17,21 @@ public class LocatingByXpathIntro {
 
         WebElement headerSpan = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/h1/span"));
         System.out.println("headerSpan.getText() = " + headerSpan.getText());
+
+        WebElement headerspan2 = driver.findElement(By.xpath("//h1/span[@class='h1y']"));
+        System.out.println("headerspan2.getText() = " + headerspan2.getText());
+
+        // Providing attributes in xpath
+        // elementTag[@attributeName='attribute value']
+        // if you have more than one
+            // elementTag[@attributeName='attribute value' and @attributeName2='attribute value']
+        // you can also provide the text() xpath function
+            // elementTag[text()='some text value']
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='box1']"));
+        System.out.println("checkbox1.getText() = " + checkbox1.getText());
+        checkbox1.click();
+
+        Thread.sleep(2500);
 
         driver.quit();
     }
