e7b4e60016b9a50a781509b45a79a8c3810fee7d
Yahya <yahyahussein91@gmail.com>
2024-04-11 11:23:09 -0700

    multiple ways of selecting searchbox

---
+++ src/test/java/com/yahya/day04_common_elements_xpath_css_intro/FindElementByCssSelector.java
@@ -0,0 +1,28 @@
+package com.yahya.day04_common_elements_xpath_css_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementByCssSelector {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://search.yahoo.com/");
+
+        // TODO: Identify searchbox using css selector
+        // Try in different ways like id, name etc
+        // WebElement searchbox = driver.findElement(By.cssSelector("#yschsp"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[name='p']"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[type='text']"));
+        // WebElement searchbox = driver.findElement(By.cssSelector("input[id='yschsp']"));
+        WebElement searchbox = driver.findElement(By.cssSelector("input[name='p'][type='text']"));
+        searchbox.sendKeys("selenium");
+
+        Thread.sleep(2500);
+        driver.quit();
+    }
+}
