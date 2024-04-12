cc679c70eddacafdd5e066f23b82e9c71800ea0b
Yahya <yahyahussein91@gmail.com>
2024-04-12 09:10:26 -0700

    selecting email by css selector

--- src/test/java/com/yahya/day05_css_xpath_junit5/CssSelectorPractice.java
+++ src/test/java/com/yahya/day05_css_xpath_junit5/CssSelectorPractice.java
@@ -1,7 +1,9 @@
 package com.yahya.day05_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssSelectorPractice {
@@ -10,6 +12,9 @@ public class CssSelectorPractice {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cydeo.com/forgot_password");
+
+        // Locate two elements using CSS Selector
+        WebElement passwordBox = driver.findElement(By.cssSelector("div>input[type='text'][name='email']"));
 
 
     }
