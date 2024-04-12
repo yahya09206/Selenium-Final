75277010c8b2f92c1b1434101983571cf73a29f2
Yahya <yahyahussein91@gmail.com>
2024-04-12 09:57:07 -0700

    added assertion to verify

--- src/test/java/com/yahya/tests/day05_css_xpath_junit5/FirstTest.java
+++ src/test/java/com/yahya/tests/day05_css_xpath_junit5/FirstTest.java
@@ -1,6 +1,18 @@
 package com.yahya.tests.day05_css_xpath_junit5;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FirstTest {
 
 
+    @Test
+    public void addition(){
+
+        int num1 = 10;
+        int num2 = 7;
+        int expected = 17;
+
+        Assertions.assertEquals(expected, num1 + num2);
+    }
 }
