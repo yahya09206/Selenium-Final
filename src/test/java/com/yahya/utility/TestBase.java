6bfafc3d9899f38ef8a78d70c2ec14e4099585ef
Yahya <yahyahussein91@gmail.com>
2024-04-15 14:28:48 -0700

    added first steps to testbase

--- src/test/java/com/yahya/utility/TestBase.java
+++ src/test/java/com/yahya/utility/TestBase.java
@@ -1,4 +1,29 @@
 package com.yahya.utility;
 
-public class TestBase {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class TestBase {
+
+    /**
+     * Everything that repeats such as setting up the webdriver/closing browser goes in this class
+     * Meant to be superclass for its subclasses
+     */
+
+    // Declare at class level
+    WebDriver driver;
+    @BeforeEach
+    public void setUpWebDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        driver.quit();
+    }
 }
