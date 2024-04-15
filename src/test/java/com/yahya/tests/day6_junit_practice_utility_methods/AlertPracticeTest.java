3de639d43518d62c03379971e70a5637f61fcc00
Yahya <yahyahussein91@gmail.com>
2024-04-15 15:25:51 -0700

    switching over to alerts

---
+++ src/test/java/com/yahya/tests/day6_junit_practice_utility_methods/AlertPracticeTest.java
@@ -0,0 +1,18 @@
+package com.yahya.tests.day6_junit_practice_utility_methods;
+
+import com.yahya.utility.TestBase;
+import org.junit.jupiter.api.Test;
+
+public class AlertPracticeTest extends TestBase {
+
+    /**
+     * Regular(ok)
+     * Confirm(ok and cancel)
+     * Prompt(enter text)
+     */
+    @Test
+    public void testJavaScriptAlert(){
+
+
+    }
+}
