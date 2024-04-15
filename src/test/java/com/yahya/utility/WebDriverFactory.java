1fe6f8803ca78d59a784a78c2661f8ce07556b85
Yahya <yahyahussein91@gmail.com>
2024-04-15 14:39:37 -0700

    setting up webdriver factory

---
+++ src/test/java/com/yahya/utility/WebDriverFactory.java
@@ -0,0 +1,17 @@
+package com.yahya.utility;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverFactory {
+
+    /**
+     * We need a utility class with methods to get WebDriver object with all the settings needed
+     * by passing browserName
+     *
+     * WebDriverFactory.get("chrome) ==> WebDriver object with Chrome Driver
+     */
+
+    public static WebDriver getBrowser(){
+
+    }
+}
