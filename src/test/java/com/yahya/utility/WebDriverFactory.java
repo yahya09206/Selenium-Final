ab7ade4277c2b31fd9038f3e90ffbe60900c1912
Yahya <yahyahussein91@gmail.com>
2024-04-15 14:42:33 -0700

    set up driver

--- src/test/java/com/yahya/utility/WebDriverFactory.java
+++ src/test/java/com/yahya/utility/WebDriverFactory.java
@@ -1,6 +1,9 @@
 package com.yahya.utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverFactory {
 
@@ -11,7 +14,26 @@ public class WebDriverFactory {
      * WebDriverFactory.get("chrome) ==> WebDriver object with Chrome Driver
      */
 
-    public static WebDriver getBrowser(){
+    public static WebDriver getDriver(String browserName){
 
+        WebDriver driver;
+
+        switch (browserName){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = null;
+                System.out.println("UNKNOWN BROWSER TYPE!!! = " + browserName);
+
+        }
+
+        driver.manage().window().maximize();
+        return driver;
     }
 }
