63c221cd40e0fca6cddbe2ced5cc9d264a8baab1
Yahya <yahyahussein91@gmail.com>
2024-04-15 13:52:38 -0700

    setting up webdriver and closing it down using annotations

---
+++ src/test/java/com/yahya/tests/day6_junit_practice_utility_methods/LoginFormPracticeTest.java
@@ -0,0 +1,25 @@
+package com.yahya.tests.day6_junit_practice_utility_methods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginFormPracticeTest {
+
+    // Declare at class level
+    WebDriver driver;
+    @BeforeEach
+    public void setUpWebDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
