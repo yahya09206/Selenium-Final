ac646e7950c60e6e7b05fd5f1af1a44329aee247
Yahya <yahyahussein91@gmail.com>
2024-04-15 14:35:40 -0700

    verified second test is working with testbase

--- src/test/java/com/yahya/tests/day6_junit_practice_utility_methods/YahooSearchTest.java
+++ src/test/java/com/yahya/tests/day6_junit_practice_utility_methods/YahooSearchTest.java
@@ -2,6 +2,9 @@ package com.yahya.tests.day6_junit_practice_utility_methods;
 
 import com.yahya.utility.TestBase;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class YahooSearchTest extends TestBase {
@@ -18,6 +21,9 @@ public class YahooSearchTest extends TestBase {
     @Test
     public void testYahooSearchResultPage(){
 
+        driver.get("https://search.yahoo.com");
+        driver.findElement(By.name("p")).sendKeys("selenium" + Keys.ENTER);
+        assertTrue(driver.getTitle().startsWith("selenium"));
 
     }
 
