012c9ec06aa3f3b18920b8ea90fb9e141227d608
Yahya <yahyahussein91@gmail.com>
2024-04-16 14:31:00 -0700

    fixed wrong xpath for file upload

--- src/test/java/com/yahya/tests/day08_upload_actions_window_wait_jse/FileUploadTest.java
+++ src/test/java/com/yahya/tests/day08_upload_actions_window_wait_jse/FileUploadTest.java
@@ -12,9 +12,11 @@ public class FileUploadTest extends TestBase {
         driver.get("https://practice.cydeo.com/upload");
         // locate the input field: By.id("file-upload)
         // By.xpath("//input[@id='file-upload']")
-        WebElement fileInputField = driver.findElement(By.xpath("\"//input[@id='file-upload']\""));
+        WebElement fileInputField = driver.findElement(By.xpath("//input[@id='file-upload']"));
         // find the full path of you file in your computer and send key into above input field
         String filePath = "/Users/solodolo/Desktop/coolimage.avif";
         fileInputField.sendKeys(filePath);
+
+        driver.findElement(By.id("file-submit")).click();
     }
 }
