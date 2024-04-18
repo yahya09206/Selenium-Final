33b14334d92da872613492f764043915ca20bd5b
Yahya <yahyahussein91@gmail.com>
2024-04-18 16:59:46 -0700

    including type of webdriver to create

--- src/test/java/com/yahya/utility/Driver.java
+++ src/test/java/com/yahya/utility/Driver.java
@@ -1,18 +1,19 @@
 package com.yahya.utility;
 
 import com.yahya.tests.day09_explicit_wait_singleton_properties.Singleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
 
-    private static Driver obj;
+    private static WebDriver obj;
 
     private Driver(){}
 
-    private static Driver getObj(){
+    private static WebDriver getDriver(){
 
         if (obj == null){
-            System.out.println("OBJECT HAS NOT BEEN CREATED IT");
-            obj = new Driver();
+            obj = new ChromeDriver();
             return obj;
         } else {
             System.out.println("YOU ALREADY HAVE OBJECT, JUST USE THAT ONE");
