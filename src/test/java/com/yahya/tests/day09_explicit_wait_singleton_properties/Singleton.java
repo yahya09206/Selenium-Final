d083d4b1eee972795938d4bb401cd6e12d443b31
Yahya <yahyahussein91@gmail.com>
2024-04-18 16:46:20 -0700

    clarifying notes

--- src/test/java/com/yahya/tests/day09_explicit_wait_singleton_properties/Singleton.java
+++ src/test/java/com/yahya/tests/day09_explicit_wait_singleton_properties/Singleton.java
@@ -1,5 +1,17 @@
 package com.yahya.tests.day09_explicit_wait_singleton_properties;
 
+/**
+ * Single design pattern to achieve single object generation
+ * Block creation of more than one object
+ *
+ * Steps to achieve
+ * 1. private constructor
+ * 2. private static field with data type same as class
+ * 3. public getter method for private field
+ *      - to check if you have already created object
+ *      - if not --> create object for the first time
+ *      - if yes --> just use existing object
+ */
 public class Singleton {
 
     // variable obj
