f6373fc756d09a0086adeff3dee90623a009823f
Yahya <yahyahussein91@gmail.com>
2024-04-18 16:53:25 -0700

    created driver with method that checks if null or not

--- src/test/java/com/yahya/utility/Driver.java
+++ src/test/java/com/yahya/utility/Driver.java
@@ -1,8 +1,24 @@
 package com.yahya.utility;
 
+import com.yahya.tests.day09_explicit_wait_singleton_properties.Singleton;
+
 public class Driver {
 
-    private Driver(){
+    private static Driver obj;
 
+    private Driver(){}
+
+    private static Driver getObj(){
+
+        if (obj == null){
+            System.out.println("OBJECT HAS NOT BEEN CREATED IT");
+            obj = new Driver();
+            return obj;
+        } else {
+            System.out.println("YOU ALREADY HAVE OBJECT, JUST USE THAT ONE");
+            return obj;
+        }
     }
+
+
 }
