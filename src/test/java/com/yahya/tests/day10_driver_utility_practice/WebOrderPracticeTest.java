7a7af34876704f9f93ba58c0cd16677be5665806
Yahya <yahyahussein91@gmail.com>
2024-04-19 10:16:33 -0700

    calling open method in test

--- src/test/java/com/yahya/tests/day10_driver_utility_practice/WebOrderPracticeTest.java
+++ src/test/java/com/yahya/tests/day10_driver_utility_practice/WebOrderPracticeTest.java
@@ -1,9 +1,14 @@
 package com.yahya.tests.day10_driver_utility_practice;
 
+import com.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
 
 public class WebOrderPracticeTest {
 
     @Test
+    public void testDriverPractice(){
+
+        WebOrderUtil.openWebOrderApp();
+    }
 
 }
--- src/test/java/com/yahya/utility/WebOrderUtil.java
+++ src/test/java/com/yahya/utility/WebOrderUtil.java
@@ -5,6 +5,7 @@ public class WebOrderUtil {
     // Method to openWebOrderApp
     public static void openWebOrderApp(){
 
+        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
 
     }
 }
