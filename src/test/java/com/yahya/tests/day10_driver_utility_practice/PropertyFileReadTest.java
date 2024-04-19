e512c78c430b3ed07fe07bccc1b3c11518ffd5ed
Yahya <yahyahussein91@gmail.com>
2024-04-19 11:49:35 -0700

    reading from config file by using input stream connection

---
+++ src/test/java/com/yahya/tests/day10_driver_utility_practice/PropertyFileReadTest.java
@@ -0,0 +1,31 @@
+package com.yahya.tests.day10_driver_utility_practice;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileReadTest {
+
+    @Test
+    public void testInitialRead() throws IOException {
+
+        /**
+         * Open a connection to the file using FileInputStream object
+         */
+        FileInputStream inputStream = new FileInputStream("config.properties");
+
+        // create properties object
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        // close the connection by calling close method of FileInputStream
+        inputStream.close();
+
+        // now actually read from the properties using its key
+        String webOrderUrl = properties.getProperty("weborder_url");
+        System.out.println(webOrderUrl);
+    }
+}
