6f1a9bea68227e75e33b6b8dc97b4b613557f8c6
Yahya <yahyahussein91@gmail.com>
2024-04-19 10:54:28 -0700

    adjusting check visibility method where to return true or false

--- src/test/java/com/yahya/utility/BrowserUtil.java
+++ src/test/java/com/yahya/utility/BrowserUtil.java
@@ -1,5 +1,13 @@
 package com.yahya.utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.sql.Time;
+import java.time.Duration;
+
 public class BrowserUtil {
 
     /**
@@ -12,6 +20,31 @@ public class BrowserUtil {
             Thread.sleep(seconds * 1000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(2));
+     *         try{
+     *             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='blablaInvalid Login or Password.']")));
+     *         } catch (TimeoutException e){
+     *             System.out.println(e.getMessage());
+     *             System.out.println("We did not see the error message element");
+     *         }
+     *
+     *         This message will check for the visibility of an element within the time given
+     * @return true if element is found within the time and visible, false if it is not
+     */
+    public static boolean checkVisibilityOfElement(By locator, Duration seconds){
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), seconds);
+
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return true;
+        } catch (TimeoutException e){
+            System.out.println("We did not see the error message element");
+            return false;
         }
     }
 }
