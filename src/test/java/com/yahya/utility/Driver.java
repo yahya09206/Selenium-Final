b7c42ea44a7e06a9b68152a1ef45e4a79dbbc99c
Yahya <yahyahussein91@gmail.com>
2024-04-18 17:02:44 -0700

    changed method from private to public

--- src/test/java/com/yahya/utility/Driver.java
+++ src/test/java/com/yahya/utility/Driver.java
@@ -1,6 +1,7 @@
 package com.yahya.utility;
 
 import com.yahya.tests.day09_explicit_wait_singleton_properties.Singleton;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,13 +11,14 @@ public class Driver {
 
     private Driver(){}
 
-    private static WebDriver getDriver(){
+    public static WebDriver getDriver(){
 
         if (obj == null){
             obj = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+            System.out.println("First and only object created");
             return obj;
         } else {
-            System.out.println("YOU ALREADY HAVE OBJECT, JUST USE THAT ONE");
             return obj;
         }
     }
