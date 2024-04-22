ce1adba24b49f0827285ef7462bd77ba31d5564f
Yahya <yahyahussein91@gmail.com>
2024-04-22 12:08:03 -0700

    setting up login test using pom

---
+++ src/test/java/com/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -0,0 +1,17 @@
+package com.yahya.tests.day11_property_driver_faker_pom;
+
+import com.yahya.pages.WLoginPage;
+import com.yahya.utility.TestBase;
+import com.yahya.utility.WebOrderUtil;
+import org.junit.jupiter.api.Test;
+
+public class WebOrderPOM_Test extends TestBase {
+
+    @Test
+    public void testWithPOM_for_login(){
+
+        WebOrderUtil.openWebOrderApp();
+        WLoginPage loginPage = new WLoginPage();
+
+    }
+}
