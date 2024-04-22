cbadcaff47c3639791b5ce9262179b5ff25ea83f
Yahya <yahyahussein91@gmail.com>
2024-04-22 13:28:23 -0700

    was using wrong selector

--- src/test/java/com/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
+++ src/test/java/com/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -1,12 +1,15 @@
 package com.yahya.tests.day11_property_driver_faker_pom;
 
+import com.github.javafaker.Faker;
 import com.yahya.pages.WAllOrderPage;
 import com.yahya.pages.WCommonArea;
 import com.yahya.pages.WLoginPage;
+import com.yahya.pages.WOrderPage;
 import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.TestBase;
 import com.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -57,6 +60,39 @@ public class WebOrderPOM_Test extends TestBase {
         BrowserUtil.waitFor(2);
 
         commonArea.viewAllOrders.click();
+        BrowserUtil.waitFor(2);
+    }
+
+    @Test
+    public void testOrderFlow(){
+
+        WLoginPage loginPage = new WLoginPage();
+        loginPage.goTo();
+        loginPage.login("Tester", "test");
+
+        WCommonArea commonArea = new WCommonArea();
+        commonArea.viewOrders.click();
+        BrowserUtil.waitFor(2);
+
+        // place order with info
+        WOrderPage orderPage = new WOrderPage();
+
+        orderPage.quantityBox.sendKeys(Keys.BACK_SPACE + "10");
+        orderPage.calculateButton.click();
+
+        Faker faker = new Faker();
+
+        orderPage.customerNameBox.sendKeys(faker.name().fullName());
+        orderPage.streetBox.sendKeys(faker.address().fullAddress());
+        orderPage.cityBox.sendKeys(faker.address().cityName());
+        orderPage.stateBox.sendKeys(faker.address().state());
+        orderPage.zipBox.sendKeys(faker.numerify("#####"));
+
+        orderPage.cardRadio.click();
+        orderPage.cardNumber.sendKeys(faker.numerify("################"));
+        orderPage.expireDate.sendKeys(faker.numerify("0#/0#"));
+
+        orderPage.processButton.click();
         BrowserUtil.waitFor(2);
     }
 }
