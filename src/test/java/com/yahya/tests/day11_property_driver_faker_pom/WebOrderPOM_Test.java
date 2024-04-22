90e682422ffab24033c9fc124c9e97d1371d6fb5
Yahya <yahyahussein91@gmail.com>
2024-04-22 12:49:48 -0700

    set up order page with methods and verified on correct page with header

--- src/test/java/com/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
+++ src/test/java/com/yahya/tests/day11_property_driver_faker_pom/WebOrderPOM_Test.java
@@ -1,10 +1,13 @@
 package com.yahya.tests.day11_property_driver_faker_pom;
 
+import com.yahya.pages.WAllOrderPage;
 import com.yahya.pages.WLoginPage;
 import com.yahya.utility.BrowserUtil;
 import com.yahya.utility.TestBase;
 import com.yahya.utility.WebOrderUtil;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WebOrderPOM_Test extends TestBase {
 
@@ -15,8 +18,26 @@ public class WebOrderPOM_Test extends TestBase {
         WLoginPage loginPage = new WLoginPage();
 
         loginPage.goTo();
-        loginPage.login("abc", "test");
+        loginPage.login("Tester", "test");
         loginPage.errorMsg.isDisplayed();
         BrowserUtil.waitFor(2);
+    }
+
+    @Test
+    public void testAllOrderPage(){
+
+        WLoginPage loginPage = new WLoginPage();
+        loginPage.goTo();
+        loginPage.login("Tester", "test");
+
+        WAllOrderPage allOrderPage = new WAllOrderPage();
+        assertTrue(allOrderPage.header.isDisplayed());
+        allOrderPage.checkAllButton.click();
+
+        BrowserUtil.waitFor(3);
+
+        allOrderPage.unCheckAllButton.click();
+
+
     }
 }
